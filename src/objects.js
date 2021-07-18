2edf0cb324dda913e5fa419c7513b3d99cfae71e
unknown <arian.angoma@tecsup.edu.pe>
2021-07-18 02:43:07 -0500

    Objects JSDoc

---
+++ src/objects.js
@@ -0,0 +1,21 @@
+/**
+ * Person Object
+ * @type {{id: number, name: string, surname: string, age: number}}
+ * */
+const person = {
+    id: 1,
+    name: 'Arian',
+    surname: 'Angoma',
+    age: 21
+}
+
+/**
+ * Person Object Two
+ * @type {{id: number | string, name: string, surname: string, age: number}}
+ * */
+const person2 = {
+    id: 1,
+    name: 'Arian',
+    surname: 'Angoma',
+    age: 21
+}
