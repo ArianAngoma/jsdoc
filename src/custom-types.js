bffcea6b66078e3f60224279684e804ebba4e917
unknown <arian.angoma@tecsup.edu.pe>
2021-07-18 03:01:23 -0500

    Custom Types JSDoc

---
+++ src/custom-types.js
@@ -0,0 +1,29 @@
+/**
+ * Nuevo objeto de usuario
+ * @typedef {Object} User
+ * @property {number} id Id de usuario
+ * @property {string} name Nombre de usuario
+ * @property {number | string} [age] Edad de usuario (Opcional)
+ * @property {boolean} isActive Estado de usuario
+ * */
+
+
+/**
+ * @type {User}
+ * */
+const myNewUser = {
+    id: 1,
+    name: "Arian",
+    age: 21,
+    isActive: true
+}
+
+/**
+ * @type {User}
+ * */
+const myNewUser2 = {
+    id: 2,
+    name: "Andrea",
+    age: 15,
+    isActive: true
+}
