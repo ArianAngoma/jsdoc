3c9e41086b953d909ce1dfdb81f9c160d7b6eff4
unknown <arian.angoma@tecsup.edu.pe>
2021-07-18 02:52:36 -0500

    Functions JSDOc

---
+++ src/functions.js
@@ -0,0 +1,17 @@
+/**
+ * Suma dos números
+ * @param {number} n1 Primer número
+ * @param {number} n2 Segundo número
+ * @return {number} Resultado total
+ * */
+const sum = (n1, n2) => {
+    return n1 + n2;
+}
+
+/**
+ * Suma dos números
+ * @param {number} n1 Primer número
+ * @param {number} n2 Segundo número
+ * @return {string} Resultado total
+ * */
+const sumTwo = (n1, n2) => `El resultado es ${n1 + n2}`;
