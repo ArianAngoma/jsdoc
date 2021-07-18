40207b8a92fd5a803492687be8405b1232ef5500
unknown <arian.angoma@tecsup.edu.pe>
2021-07-18 01:56:57 -0500

    Arrays JSDoc

---
+++ src/arrays.js
@@ -0,0 +1,17 @@
+/**
+ * Lista de Edades de usuarios
+ * @type {Array}
+ * */
+const age = [10, 11, 12, 13, 14, 15];
+
+/**
+ * Lista de Puntajes de usuarios
+ * @type {Array<Number>}
+ * */
+const score = [100, 200, 300, 400, 500];
+
+/**
+ * Arreglo de Números y Strings
+ * @type {Array<Number | string>}
+ * */
+const numberAndString = [10, "Arian", 20, "Angoma", 30, "Vilchez"];
