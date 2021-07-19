e87ecb52b88e121603226be2255803125a1df9b3
unknown <arian.angoma@tecsup.edu.pe>
2021-07-19 02:13:42 -0500

    Modules JSDoc

--- src/functions.js
+++ src/functions.js
@@ -1,8 +1,13 @@
 /**
+ * Librería de operaciones matemáticas
+ * @module Math
+ * */
+
+/**
  * Suma dos números
  * @param {number} n1 Primer número
  * @param {number} n2 Segundo número
- * @return {number} Resultado total
+ * @returns {number} Resultado total
  * */
 const sum = (n1, n2) => {
     return n1 + n2;
@@ -12,6 +17,6 @@ const sum = (n1, n2) => {
  * Suma dos números
  * @param {number} n1 Primer número
  * @param {number} n2 Segundo número
- * @return {string} Resultado total
+ * @returns {string} Resultado total
  * */
 const sumTwo = (n1, n2) => `El resultado es ${n1 + n2}`;
---
+++ src/libs/math.js
@@ -0,0 +1,11 @@
+/**
+ * Suma dos números
+ * @param {number} x Primer número
+ * @param {number} y Segundo número
+ * @returns {number} Suma de x, y
+ * */
+const sum = (x, y) => x + y;
+
+module.exports = {
+    sum
+}
